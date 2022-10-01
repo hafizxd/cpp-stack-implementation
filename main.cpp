5506c319bc5f8cfacbed39b363c802c3113d55ac
Hafiz Aminsalim <hafizamin8116@gmail.com>
2022-10-01 09:57:29 +0700

    fix

--- main.cpp
+++ main.cpp
@@ -1,15 +1,4 @@
 #include<iostream>
-#include<curses.h>
-#include<windows.h>
-
-void gotoxy(int x, int y)
-{
-    COORD coord;
-    coord.X=x;
-    coord.Y=y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
-}
-
 
 using namespace std;
 struct paket
@@ -24,17 +13,6 @@ int top = 1;
 int maxSize = 10;
 
 /*******************************************/
-void bersihlayar()
-{
-    for(int a=0;a<80;a++)
-    {
-        for(int b=0;b<=24;b++)
-        {
-            gotoxy(a,b);cout<<" ";
-        }
-    }
-}
-
 bool IsFull() {
     return top > maxSize;
 }
@@ -49,13 +27,13 @@ void Push()
         cout << "Stack is full" << endl;
         return;
     }
-    int x = top-1;
+    int x = top;
 
-    gotoxy(20,15);cout<<"Inputkan Jenis Barang    :";
+    cout<<"Inputkan Jenis Barang    : ";
     cin>>kirim[x].jenis_brg;
-    gotoxy(20,16);cout<<"Inputkan Berat           :";
+    cout<<"Inputkan Berat           : ";
     cin>>kirim[x].berat;
-    gotoxy(20,17);cout<<"Inputkan Kota Tujuan     :";
+    cout<<"Inputkan Kota Tujuan     : ";
     cin>>kirim[x].kota;
 
     top++;
@@ -69,13 +47,12 @@ void Pop()
     }
     int x = top-1;
 
-    gotoxy(40,12);cout<<"Data Terakhir yang anda Hapus Adalah :"<<endl;
-    gotoxy(40,13);cout<<kirim[x].jenis_brg<<" ";
+    cout<<"Data Terakhir yang anda Hapus Adalah :"<<endl;
+    cout<<kirim[x].jenis_brg<<" ";
     cout<<kirim[x].berat<<" Kg ";
     cout<<kirim[x].kota<<endl<<endl;
 
-    gotoxy(40,15);cout<<"Press Any Key...";
-    getch();
+    top--;
 }
 
 void Display()
@@ -85,18 +62,14 @@ void Display()
         return;
     }
 
-    gotoxy(40,10);cout<<"Daftar Kiriman Paket"<<endl;
+    cout<<"Daftar Kiriman Paket"<<endl;
     for(int a=1; a<top; a++)
     {
-        gotoxy(40,19-a);
         cout<<a<<" ";
         cout<<kirim[a].jenis_brg<<" ";
         cout<<kirim[a].berat<<" ";
         cout<<kirim[a].kota<<endl;
     }
-    gotoxy(40,20);
-    cout<<"Press Any Key...";
-    getch();
     /*harus Ijin --> #include<conio.h>*/
 }
 
@@ -107,17 +80,14 @@ void Top() {
     }
     int x = top-1;
 
-    gotoxy(40,13);
     cout << "Top is " << kirim[x].jenis_brg<<" ";
     cout<<kirim[x].berat<<" Kg ";
     cout<<kirim[x].kota<<endl<<endl;
 
-    gotoxy(40,15);cout<<"Press Any Key...";
-    getch();
 }
 
 void Size() {
-    cout << "Size of Stack is " << top-1;
+    cout << "Size of Stack is " << top-1 << endl;
 }
 
 int main()
@@ -125,18 +95,18 @@ int main()
     int pilih;
     do
     {
-        bersihlayar();
-        gotoxy(1,1);cout<<"MENU UTAMA";
-        gotoxy(1,2);cout<<"1. PUSH"<<endl;
-        gotoxy(1,3);cout<<"2. POP"<<endl;
-        gotoxy(1,4);cout<<"3. DISPLAY"<<endl;
-        gotoxy(1,5);cout<<"4. ISFULL"<<endl;
-        gotoxy(1,6);cout<<"5. ISEMPTY"<<endl;
-        gotoxy(1,7);cout<<"6. TOP"<<endl;
-        gotoxy(1,8);cout<<"7. SIZE"<<endl;
-        gotoxy(1,9);cout<<"8. EXIT"<<endl;
 
-        gotoxy(1,10);cout<<"Pilihan Anda [1-9] : ";
+        cout<<"MENU UTAMA" << endl;
+        cout<<"1. PUSH"<<endl;
+        cout<<"2. POP"<<endl;
+        cout<<"3. DISPLAY"<<endl;
+        cout<<"4. ISFULL"<<endl;
+        cout<<"5. ISEMPTY"<<endl;
+        cout<<"6. TOP"<<endl;
+        cout<<"7. SIZE"<<endl;
+        cout<<"8. EXIT"<<endl;
+
+        cout<<"Pilihan Anda [1-9] : ";
         cin>>pilih;
         switch(pilih) {
             case 1:
